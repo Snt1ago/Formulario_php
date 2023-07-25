afd889ec2fbf4840ea087ecef3b42d76bad44e2d
Leandro Calvetti <lcalvetti1@gmail.com>
2023-07-25 14:18:10 -0300

    agreue recepcion de checkbox

--- procesar.php
+++ procesar.php
@@ -2,22 +2,22 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
 
 <body>
-    <main>
-        <section>
-            <header>
-                <h1>Mostrar datos</h1>
-            </header>
-            <?php
+  <main>
+    <section>
+      <header>
+        <h1>Mostrar datos</h1>
+      </header>
+      <?php
             $nombre = $_POST['nombre'];
             $vacio = $_POST['hidden'];
             $opciones = $_POST['color'];
-            $comida = $_POST['check'];
+            $comidas = $_POST['check'];
             $mensaje = $_POST['mensaje'];
             $paises = $_POST['pais'];
 
@@ -25,13 +25,16 @@
             echo "Nombre: " . $nombre . "<br>";
             echo "Vacio: " . $vacio . "<br>";
             echo "Opciones: " . $opciones . "<br>";
-            echo "Comida: " . $comida . "<br>";
+            //echo "Comida: " . $comida . "<br>";
+            foreach ($comidas as $comida) {
+                echo "<p>- $comida</p>";
+            }
             echo "Mensaje: " . $mensaje . "<br>";
             echo "Paises: " . $paises;
 
             ?>
-        </section>
-    </main>
+    </section>
+  </main>
 </body>
 
 </html>
