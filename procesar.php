4cd1eb71fcdfd94782f2b33b93457a82a9131b8a
Leandro Calvetti <lcalvetti1@gmail.com>
2023-07-25 15:00:28 -0300

    correccion p02

--- procesar.php
+++ procesar.php
@@ -2,18 +2,18 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 
 <body>
-  <main>
-    <section>
-      <header>
-        <h1>Mostrar datos</h1>
-      </header>
-      <?php
+    <main>
+        <section>
+            <header>
+                <h1>Mostrar datos</h1>
+            </header>
+            <?php
             $nombre = $_POST['nombre'];
             $vacio = $_POST['hidden'];
             $opciones = $_POST['color'];
@@ -33,8 +33,8 @@
             echo "Paises: " . $paises;
 
             ?>
-    </section>
-  </main>
+        </section>
+    </main>
 </body>
 
 </html>
